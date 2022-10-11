1ee880c68723bb713fd04a987ea6d7943a0e13b5
QDUNI <57755537+QDUNI@users.noreply.github.com>
2022-10-11 13:15:46 +0200

    renaming

--- src/action.ts
+++ src/action.ts
@@ -1,4 +1,16 @@
-export async function main(): Promise<void> {
+import {pre} from './pre'
+import {data} from './getdata'
+import {post} from './post'
 
+
+export async function main(): Promise<void> {
+    try {
     
+        pre()  // call preconditions check. 
+        data() // call data check. 
+        post() // call post check. 
+        
+    } catch (error) {
+        // catch error
+    }
 }
---
+++ src/getdata.ts
@@ -0,0 +1,9 @@
+export async function data(): Promise<object> {
+
+    
+
+
+
+
+    return Object
+}
