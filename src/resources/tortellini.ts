51f6ad10e54360df03044651b69e3e59b33b733f
JarnoHendriksen <jarno.hendriksen@gmail.com>
2022-10-14 00:11:14 +0200

    FEAT: Extract and parse yaml file from tortellini artifact

--- src/resources/tortellini.ts
+++ src/resources/tortellini.ts
@@ -1,7 +1,23 @@
 import { ReturnObject } from "../getdata";
+import * as artifact from "@actions/artifact";
+import * as fs from "fs";
+import YAML from "yaml";
+
 export async function runTortellini(): Promise<ReturnObject> {
+    const artifactClient: artifact.ArtifactClient = artifact.create();
+    const downloadResponse = await artifactClient.downloadArtifact(
+        "tortellini-result",
+        "tortellini-artifact/out"
+    );
+
+    const buffer = fs.readFileSync(
+        downloadResponse.downloadPath + "/evaluation-result.yml"
+    );
+    const fileContents = buffer.toString();
+
+    const obj = YAML.parse(fileContents);
     return {
         ReturnName: "Tortellini",
-        ReturnData: {},
+        ReturnData: obj,
     };
 }
