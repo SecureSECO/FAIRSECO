617a1feb82f75a0be987158dc96185d3c98448d4
rowins <boyrowin@gmail.com>
2022-10-18 16:18:40 +0200

    test repos input

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,14 @@
 import { pre } from "./pre";
 import { data } from "./getdata";
 import { post } from "./post";
+import * as core from '@actions/core'
 
 export async function main(): Promise<void> {
     try {
         const repository: string = core.getInput('repository')
+        console.log(repository)
         pre(); // call preconditions check.
-        const result = await data(repository); // call data check.
+        const result = await data(); // call data check.
         post(result); // call post check.
     } catch (error) {
         // catch error
