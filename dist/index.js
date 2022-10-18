a632116abed0a686c7a922ad578df231c1130657
rowins <boyrowin@gmail.com>
2022-10-18 15:44:27 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1415,29 +1415,6 @@ exports.pre = pre;
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -1450,13 +1427,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.runHowfairis = void 0;
 const exec_1 = __nccwpck_require__(9710);
-const fs = __importStar(__nccwpck_require__(7147));
 function runHowfairis() {
     return __awaiter(this, void 0, void 0, function* () {
         console.log("howfairis started");
-        const fileName = "./howfairis-output.json";
         const cmd = "docker";
-        const args = ["run", "--rm", "fairsoftware/fairtally", "--format", "json", "-o", "-", "https://github.com/fair-software/fairtally", ">", fileName];
+        const args = ["run", "--rm", "fairsoftware/fairtally", "--format", "json", "-o", "-", "https://github.com/fair-software/fairtally"];
         let stdout = "";
         let stderr = "";
         const options = {
@@ -1474,11 +1449,10 @@ function runHowfairis() {
         };
         const exitCode = yield (0, exec_1.exec)(cmd, args, options);
         console.log("docker running fairtally returned " + exitCode);
-        let jsonOutput = fs.readFileSync(fileName, "utf8");
-        console.log(jsonOutput);
+        console.log(stdout);
         return {
             ReturnName: "HowFairIs",
-            ReturnData: JSON.parse(jsonOutput)
+            ReturnData: JSON.parse(stdout)
         };
     });
 }
