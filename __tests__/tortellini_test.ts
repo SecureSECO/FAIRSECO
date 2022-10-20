a08cffdb667086ffedc3ce31dc1df97bfc90b3b6
JarnoHendriksen <jarno.hendriksen@gmail.com>
2022-10-20 12:33:30 +0200

    Clean up tortellini.ts and tortellini_test.ts

--- __tests__/tortellini_test.ts
+++ __tests__/tortellini_test.ts
@@ -1,8 +1,5 @@
 import { runTortellini, functionsToTest } from "../src/resources/tortellini";
 import { expect, test } from "@jest/globals";
-import * as fs from "fs";
-import * as artifact from "@actions/artifact";
-import YAML from "yaml";
 
 const { getArtifactData, getFileFromArtifact } = functionsToTest;
 
@@ -28,27 +25,3 @@ test("Test if runTortellini returns a correct ReturnObject", async () => {
     expect(result.ReturnName).toBe("Tortellini");
     expect(result.ReturnData).not.toBeUndefined();
 });
-
-function uploadDummyArtifact(
-    artName: string,
-    location: string,
-    content: string,
-    fileName: string
-) {
-    try {
-        fs.mkdirSync(location);
-    } catch {
-        console.log("Folder " + location + " already exists");
-    }
-    const fd: number = fs.openSync(location + "/" + fileName, "w+");
-    try {
-        //console.log(result);
-        fs.writeSync(fd, content);
-        //console.log("Successfully wrote YML file to dir");
-        fs.closeSync(fd);
-    } catch {
-        console.error("Error writing file");
-    }
-    const artClient = artifact.create();
-    artClient.uploadArtifact(artName, [location + "/" + fileName], location);
-}
