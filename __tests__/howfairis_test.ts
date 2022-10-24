0aae4d543fcc2c6f2413f27a86ca1da7e4f49d82
QDUNI <57755537+QDUNI@users.noreply.github.com>
2022-10-24 13:35:36 +0200

    TEST | @howfairis

--- __tests__/howfairis_test.ts
+++ __tests__/howfairis_test.ts
@@ -1,43 +1,32 @@
-import Ajv, {JSONSchemaType} from "ajv"
-const ajv = new Ajv()
-import  * as fs from "fs";
-
-//use interface?
-
-const schema: JSONSchemaType = {
-  type: "object",
-  properties: {
-    badge: {type: "string"}, 
-    checklist: {type: "bool"}, 
-    citation: {type: "bool"}, 
-    count: {type: "integer", minimum: 0, maximum: 5}, 
-    license: {type: "bool"}, 
-    registry: {type: "bool"}, 
-    repository: {type: "bool"}, 
-    stderr: {type: "string",nullable: true}, 
-    stdout: {type: "string",nullable: true}, 
-    url: {type: "string"}
-  },
-  required: ["badge","checklist","citation","count","license","registry","repository","stderr","stdout","url"],
-  additionalProperties: false
-}
-
-// validate is a type guard for MyData - type is inferred from schema type
-const validate = ajv.compile(schema)
-
-// or, if you did not use type annotation for the schema,
-// type parameter can be used to make it type guard:
-// const validate = ajv.compile<MyData>(schema)
-
-const file = fs.readFileSync("./.FairSECO/HowFairIs.JSON");
-const data = JSON.parse(file)
-
-if (validate(data)) {
-  // data is MyData here
-  console.log("HowFairIs test passed")
-} else {
-  console.log(validate.errors)
-}
+import { runHowfairis } from '../src/resources/howfairis';
+import { ReturnObject } from "../src/getdata";
+import { matchers } from 'jest-json-schema';
 
 
-// source: https://ajv.js.org/guide/typescript.html
+expect.extend(matchers);
+
+
+test("that output json matches the schema", async () =>{
+
+  const schema = {
+    properties: {
+      badge: {type: "string"}, 
+      checklist: {type: "bool"}, 
+      citation: {type: "bool"}, 
+      count: {type: "integer", minimum: 0, maximum: 5}, 
+      license: {type: "bool"}, 
+      registry: {type: "bool"}, 
+      repository: {type: "bool"}, 
+      stderr: {type: "string",nullable: true}, 
+      stdout: {type: "string",nullable: true}, 
+      url: {type: "string"}
+    },
+    required: ["badge","checklist","citation","count","license","registry","repository","stderr","stdout","url"],
+    additionalProperties: false
+  }
+
+  // const githubtesturl = 'https://github.com/QDUNI/FairSECO/';
+  const outputmodule : ReturnObject = await runHowfairis();
+  expect(outputmodule).toMatchSchema(schema);
+  
+});
