8fbe9be485aafcde1e9f6cfa127283650d15188f
Jarno Hendriksen <jarno.hendriksen@gmail.com>
2022-10-24 16:51:24 +0200

    Tortellini License Checking (#2)
    * TEST: create mock of github actions artifact
    
    * FIX: filter Tortellini output
    
    * Update tortellini.ts
    
    * CHORE: remove Tortellini from test workflow
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * Update index.js
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * Update tortellini.ts
    
    * TEST: update tortellini_test so it checks if all properties are in the returned object
    
    * Update tortellini_test.ts
    
    * Update index.js
    
    * Update tortellini_test.ts
    
    * Update tortellini_test.ts
    
    * update test
    
    * Update tortellini_test.ts
    
    * CHORE: add comments
    
    Co-authored-by: Quinn Donkers <57755537+QDUNI@users.noreply.github.com>

--- __tests__/tortellini_test.ts
+++ __tests__/tortellini_test.ts
@@ -1,14 +1,29 @@
-import { runTortellini, functionsToTest } from "../src/resources/tortellini";
+import * as tort from "../src/resources/tortellini";
 import { expect, test } from "@jest/globals";
 
-const { getArtifactData, getFileFromArtifact } = functionsToTest;
+const mockArtifact = createMockArtifact();
 
-test("Test retrieval of artifacts", async () => {
-    const dlResponse = await getArtifactData(
+test("Check if a correct downloadResponse is generated", async () => {
+    const dlResponse = await tort.getArtifactData(
         "tortellini-result",
-        ".tortellini-artifact"
+        ".tortellini-unit-test",
+        mockArtifact
     );
-    const result = await getFileFromArtifact(
+
+    expect(dlResponse).toEqual({
+        artifactName: "tortellini-result",
+        downloadPath: ".tortellini-unit-test",
+    });
+});
+
+test("Check if a file can be retrieved with the downloadResponse", async () => {
+    const dlResponse = await tort.getArtifactData(
+        "tortellini-result",
+        ".tortellini-unit-test",
+        mockArtifact
+    );
+
+    const result = await tort.getFileFromArtifact(
         dlResponse,
         "evaluation-result.yml"
     );
@@ -19,9 +34,60 @@ test("Test retrieval of artifacts", async () => {
 });
 
 test("Test if runTortellini returns a correct ReturnObject", async () => {
-    const result = await runTortellini();
+    const result = await tort.runTortellini(mockArtifact);
+
+    const data: any = result.ReturnData;
 
     expect(result).not.toBeUndefined();
     expect(result.ReturnName).toBe("Tortellini");
-    expect(result.ReturnData).not.toBeUndefined();
+    expect(data).toHaveProperty("project");
+    const proj = data.project;
+    expect(proj).toHaveProperty("id");
+    expect(proj).toHaveProperty("licenses");
+    expect(proj).toHaveProperty("authors");
+    expect(proj).toHaveProperty("vcs");
+    expect(proj.vcs).toHaveProperty("url");
+
+    expect(data).toHaveProperty("packages");
+    const pack = data.packages[0];
+    expect(pack).toHaveProperty("id");
+    expect(pack).toHaveProperty("licenses");
+    expect(pack).toHaveProperty("description");
+    expect(pack).toHaveProperty("authors");
+    expect(pack).toHaveProperty("vcs");
+    expect(pack.vcs).toHaveProperty("url");
+
+    expect(data).toHaveProperty("violations");
+    const viol = data.violations[0];
+    expect(viol).toHaveProperty("rule");
+    expect(viol).toHaveProperty("pkg");
+    expect(viol).toHaveProperty("license");
+    expect(viol).toHaveProperty("license_source");
+    expect(viol).toHaveProperty("severity");
+    expect(viol).toHaveProperty("message");
+    expect(viol).toHaveProperty("how_to_fix");
 });
+
+function createMockArtifact(): tort.Artifact {
+    // Create DLArtFunc
+    const downloadArt: tort.DLArtFunc = function (
+        name: string,
+        path?: string | undefined,
+        options?: tort.DownloadOptions
+    ) {
+        return { artifactName: name, downloadPath: path };
+    };
+
+    // Create TestClient
+    const client: tort.TestClient = { downloadArtifact: downloadArt };
+
+    // Create create function
+    const create_ = function () {
+        return client;
+    };
+
+    const testArt: tort.TestArtifact = { create: create_ };
+
+    // Create TestArtifact
+    return testArt;
+}
