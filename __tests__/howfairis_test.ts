059c8e1054527559e0bb9c2cf530c8e9de9b086b
Bart Hageman <barthageman1@gmail.com>
2022-10-24 14:47:30 +0200

    TEST: Fix schema in howfairis test

--- __tests__/howfairis_test.ts
+++ __tests__/howfairis_test.ts
@@ -6,34 +6,65 @@ jest.setTimeout(30000);
 
 test("that output json matches the schema", async () => {
     const schema = {
-        properties: {
-            badge: { type: "string" },
-            checklist: { type: "bool" },
-            citation: { type: "bool" },
-            count: { type: "integer", minimum: 0, maximum: 5 },
-            license: { type: "bool" },
-            registry: { type: "bool" },
-            repository: { type: "bool" },
-            stderr: { type: "string", nullable: true },
-            stdout: { type: "string", nullable: true },
-            url: { type: "string" },
+        definitions: {
+            howfairis: {
+                type: "object",
+                additionalProperties: false,
+                properties: {
+                    badge: {
+                        type: "string",
+                        format: "uri",
+                        "qt-uri-protocols": ["https"],
+                    },
+                    checklist: {
+                        type: "boolean",
+                    },
+                    citation: {
+                        type: "boolean",
+                    },
+                    count: {
+                        type: "integer",
+                    },
+                    license: {
+                        type: "boolean",
+                    },
+                    registry: {
+                        type: "boolean",
+                    },
+                    repository: {
+                        type: "boolean",
+                    },
+                    stderr: {
+                        type: "string",
+                    },
+                    stdout: {
+                        type: "string",
+                    },
+                    url: {
+                        type: "string",
+                        format: "uri",
+                        "qt-uri-protocols": ["https"],
+                    },
+                },
+                required: [
+                    "badge",
+                    "checklist",
+                    "citation",
+                    "count",
+                    "license",
+                    "registry",
+                    "repository",
+                    "stderr",
+                    "stdout",
+                    "url",
+                ],
+                title: "howfairis",
+            },
         },
-        required: [
-            "badge",
-            "checklist",
-            "citation",
-            "count",
-            "license",
-            "registry",
-            "repository",
-            "stderr",
-            "stdout",
-            "url",
-        ],
-        additionalProperties: false,
     };
 
     // const githubtesturl = 'https://github.com/QDUNI/FairSECO/';
     const outputmodule: ReturnObject = await runHowfairis();
+    console.log(outputmodule.ReturnData);
     expect(outputmodule).toMatchSchema(schema);
 });
