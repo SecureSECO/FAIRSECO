b9d925aaf2b9e2ea210838d5e6e4a9abe39933a4
Bart Hageman <barthageman1@gmail.com>
2022-10-24 13:52:05 +0200

    FIX: Fix howfairis test timing out because it's long

--- __tests__/howfairis_test.ts
+++ __tests__/howfairis_test.ts
@@ -1,32 +1,39 @@
-import { runHowfairis } from '../src/resources/howfairis';
+import { runHowfairis } from "../src/resources/howfairis";
 import { ReturnObject } from "../src/getdata";
-import { matchers } from 'jest-json-schema';
-
-
+import { matchers } from "jest-json-schema";
 expect.extend(matchers);
+jest.setTimeout(10000);
 
+test("that output json matches the schema", async () => {
+    const schema = {
+        properties: {
+            badge: { type: "string" },
+            checklist: { type: "bool" },
+            citation: { type: "bool" },
+            count: { type: "integer", minimum: 0, maximum: 5 },
+            license: { type: "bool" },
+            registry: { type: "bool" },
+            repository: { type: "bool" },
+            stderr: { type: "string", nullable: true },
+            stdout: { type: "string", nullable: true },
+            url: { type: "string" },
+        },
+        required: [
+            "badge",
+            "checklist",
+            "citation",
+            "count",
+            "license",
+            "registry",
+            "repository",
+            "stderr",
+            "stdout",
+            "url",
+        ],
+        additionalProperties: false,
+    };
 
-test("that output json matches the schema", async () =>{
-
-  const schema = {
-    properties: {
-      badge: {type: "string"}, 
-      checklist: {type: "bool"}, 
-      citation: {type: "bool"}, 
-      count: {type: "integer", minimum: 0, maximum: 5}, 
-      license: {type: "bool"}, 
-      registry: {type: "bool"}, 
-      repository: {type: "bool"}, 
-      stderr: {type: "string",nullable: true}, 
-      stdout: {type: "string",nullable: true}, 
-      url: {type: "string"}
-    },
-    required: ["badge","checklist","citation","count","license","registry","repository","stderr","stdout","url"],
-    additionalProperties: false
-  }
-
-  // const githubtesturl = 'https://github.com/QDUNI/FairSECO/';
-  const outputmodule : ReturnObject = await runHowfairis();
-  expect(outputmodule).toMatchSchema(schema);
-  
+    // const githubtesturl = 'https://github.com/QDUNI/FairSECO/';
+    const outputmodule: ReturnObject = await runHowfairis();
+    expect(outputmodule).toMatchSchema(schema);
 });
