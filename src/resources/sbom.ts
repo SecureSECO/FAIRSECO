ecb3dbd3eb975ab3fba8acbfa74d07e325a7b8c0
rowins <boyrowin@gmail.com>
2022-10-27 14:17:34 +0200

    -

--- src/resources/sbom.ts
+++ src/resources/sbom.ts
@@ -1,8 +1,35 @@
 import { ReturnObject } from "../getdata";
+import { Artifact, getArtifactData, getFileFromArtifact } from "./tortellini";
+import * as artifact from "@actions/artifact";
+import YAML from "yaml";
 
-export async function runSBOM(): Promise<ReturnObject> {
+export async function runSBOM(
+    artifactObject?: Artifact
+    ): Promise<ReturnObject> {
+    let destination: string = "";
+    if (artifactObject !== undefined) {
+        destination = ".tortellini-unit-test";
+    } else {
+        artifactObject = artifact;
+        destination = ".SBOM-artifact";
+    }
+
+    const downloadResponse = await getArtifactData(
+        "SBOM.spdx",
+        destination,
+        artifactObject
+    );
+
+    const fileContents = await getFileFromArtifact(
+        downloadResponse,
+        "SBOM.spdx"
+    );
+
+    const obj = YAML.parse(fileContents);
+
     return {
         ReturnName: "SBOM",
-        ReturnData: {},
+        ReturnData: obj,
     };
 }
+
