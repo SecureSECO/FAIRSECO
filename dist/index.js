a757b95d13549edcf1c95ba3c75171fa76590453
rowins <boyrowin@gmail.com>
2022-10-27 11:17:35 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -11188,46 +11188,90 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.functionsToTest = exports.runTortellini = void 0;
+exports.filterData = exports.getFileFromArtifact = exports.getArtifactData = exports.runTortellini = void 0;
 const artifact = __importStar(__nccwpck_require__(9151));
 const fs = __importStar(__nccwpck_require__(7147));
 const path = __importStar(__nccwpck_require__(1017));
 const yaml_1 = __importDefault(__nccwpck_require__(1317));
-function runTortellini() {
+function runTortellini(artifactObject) {
     return __awaiter(this, void 0, void 0, function* () {
-        const downloadResponse = yield getArtifactData("tortellini-result", ".tortellini-artifact");
+        // An artifact object is only passed in the unit test. If that is the case,
+        // set the download destination to the unit test output folder.
+        // If not, use the regular Github Action artifact, and the normal output folder
+        let destination = "";
+        if (artifactObject !== undefined) {
+            destination = ".tortellini-unit-test";
+        }
+        else {
+            artifactObject = artifact;
+            destination = ".tortellini-artifact";
+        }
+        const downloadResponse = yield getArtifactData("tortellini-result", destination, artifactObject);
         const fileContents = yield getFileFromArtifact(downloadResponse, "evaluation-result.yml");
         const obj = yaml_1.default.parse(fileContents);
+        const filteredData = yield filterData(obj);
         return {
             ReturnName: "Tortellini",
-            ReturnData: {
-                result: obj.analyzer.result,
-                violations: obj.evaluator.violations,
-            },
+            ReturnData: filteredData,
         };
     });
 }
 exports.runTortellini = runTortellini;
 // Download the artifact that was uploaded by Tortellini
-function getArtifactData(artifactName, destination) {
+function getArtifactData(artifactName, destination, artifactObject) {
     return __awaiter(this, void 0, void 0, function* () {
-        const artifactClient = artifact.create();
+        const artifactClient = artifactObject.create();
         const downloadResponse = yield artifactClient.downloadArtifact(artifactName, destination);
         return downloadResponse;
     });
 }
+exports.getArtifactData = getArtifactData;
 // Get a file from the artifact as a string
 function getFileFromArtifact(dlResponse, fileName) {
     return __awaiter(this, void 0, void 0, function* () {
-        const filePath = path.join(dlResponse.downloadPath, fileName);
+        let filePath = "";
+        if (dlResponse.downloadPath === undefined)
+            filePath = fileName;
+        else
+            filePath = path.join(dlResponse.downloadPath, fileName);
         const buffer = fs.readFileSync(filePath);
         return buffer.toString();
     });
 }
-exports.functionsToTest = {
-    getArtifactData,
-    getFileFromArtifact,
-};
+exports.getFileFromArtifact = getFileFromArtifact;
+// Only get the data that is relevant for license checking
+// To make sure all properties are always present,
+// replace undefined properties with a dash
+function filterData(obj) {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Project data
+        const project = obj.analyzer.result.projects[0];
+        const projData = {
+            id: project.id || "-",
+            licenses: project.declared_licenses || "-",
+            description: project.description || "-",
+            authors: project.authors || "-",
+            vcs: project.vcs_processed || "-",
+        };
+        // Package data
+        const packages = obj.analyzer.result.packages;
+        const packData = [];
+        for (const pack of packages) {
+            const p = {
+                id: pack.package.id || "-",
+                licenses: pack.package.declared_licenses || "-",
+                description: pack.package.description || "-",
+                authors: pack.package.authors || "-",
+                vcs: pack.package.vcs_processed || "-",
+            };
+            packData.push(p);
+        }
+        // Violations
+        const viol = obj.evaluator.violations;
+        return { project: projData, packages: packData, violations: viol };
+    });
+}
+exports.filterData = filterData;
 
 
 /***/ }),
