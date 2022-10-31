57e782419f01a97349027e3bf56063f6fd3f0609
rowins <boyrowin@gmail.com>
2022-10-31 16:02:38 +0100

    Update sbom_test.ts

--- __tests__/sbom_test.ts
+++ __tests__/sbom_test.ts
@@ -3,20 +3,5 @@ import { ReturnObject } from '../src/getdata';
 import { runSBOM } from "../src/resources/sbom";
 
 test("Check if sbom file exists", async () => {
-    let fileExists: Boolean = false;
-    let mockResult: ReturnObject[] = []
-    let mockSingleResult: ReturnObject = {
-        ReturnName: "test",
-        ReturnData: {}
-    }
-    mockResult[0] = mockSingleResult;
-
-    runSBOM().then(() => {
-        fileExists = fs.existsSync("./.SBOM-unit-test/SBOM.spdx");
-        console.log("exists")
-        expect(fileExists).toBe(true);
-    }).catch(() => {
-        console.log("does not exist")
-        expect(fileExists).toBe(true);
-    })
+    const outputmodule: ReturnObject = await runSBOM();    
 });
