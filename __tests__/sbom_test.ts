2c939082a4ab5c6f93067ff4f105f2f2ac02574d
rowins <boyrowin@gmail.com>
2022-10-31 16:16:27 +0100

    Update sbom_test.ts

--- __tests__/sbom_test.ts
+++ __tests__/sbom_test.ts
@@ -1,7 +1,49 @@
 import * as fs from 'fs'
 import { ReturnObject } from '../src/getdata';
 import { runSBOM } from "../src/resources/sbom";
+import * as art from "../src/resources/helperfunctions/artifact"
+
+const mockArtifact = createMockArtifact();
 
 test("Check if sbom file exists", async () => {
-    const outputmodule: ReturnObject = await runSBOM();    
+    const outputmodule: ReturnObject = await runSBOM(mockArtifact);
+    
+    let fileExists: Boolean = false;
+    let mockResult: ReturnObject[] = []
+    let mockSingleResult: ReturnObject = {
+        ReturnName: "test",
+        ReturnData: {}
+    }
+    mockResult[0] = mockSingleResult;
+
+    runSBOM(mockArtifact).then(() => {
+        fileExists = fs.existsSync("./.SBOM-unit-test/SBOM.spdx");
+        expect(fileExists).toBe(true);
+    }).catch(() => {
+        expect(fileExists).toBe(true);
+    })
 });
+
+function createMockArtifact(): art.Artifact {
+    // Create DLArtFunc
+    const downloadArt: art.DLArtFunc = function (
+        name: string,
+        path?: string | undefined,
+        options?: art.DownloadOptions
+    ) {
+        return { artifactName: name, downloadPath: path };
+    };
+
+    // Create TestClient
+    const client: art.TestClient = { downloadArtifact: downloadArt };
+
+    // Create create function
+    const create_ = function () {
+        return client;
+    };
+
+    const testArt: art.TestArtifact = { create: create_ };
+
+    // Create TestArtifact
+    return testArt;
+}
