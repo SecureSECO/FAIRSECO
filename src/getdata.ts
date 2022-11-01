c038c6772f61725e0af6d300b537c91cbbba6826
JarnoHendriksen <jarno.hendriksen@gmail.com>
2022-11-01 13:53:01 +0100

    citation.cff finished

--- src/getdata.ts
+++ src/getdata.ts
@@ -10,29 +10,29 @@ export interface ReturnObject {
 export async function data(): Promise<ReturnObject[]> {
     const output: ReturnObject[] = [];
 
-    // try {
-    //     const tortelliniResult = await runTortellini();
-    //     output.push(tortelliniResult);
-    // } catch (error) {
-    //     console.error("Tortellini threw an error:");
-    //     console.error(error);
-    // }
+    try {
+        const tortelliniResult = await runTortellini();
+        output.push(tortelliniResult);
+    } catch (error) {
+        console.error("Tortellini threw an error:");
+        console.error(error);
+    }
 
-    // try {
-    //     const howfairisResult = await runHowfairis();
-    //     output.push(howfairisResult);
-    // } catch (error) {
-    //     console.error("Howfairis threw an error:");
-    //     console.error(error);
-    // }
+    try {
+        const howfairisResult = await runHowfairis();
+        output.push(howfairisResult);
+    } catch (error) {
+        console.error("Howfairis threw an error:");
+        console.error(error);
+    }
 
-    // try {
-    //     const searchsecoResult = await runSearchseco();
-    //     output.push(searchsecoResult);
-    // } catch (error) {
-    //     console.error("Searchseco threw an error:");
-    //     console.error(error);
-    // }
+    try {
+        const searchsecoResult = await runSearchseco();
+        output.push(searchsecoResult);
+    } catch (error) {
+        console.error("Searchseco threw an error:");
+        console.error(error);
+    }
 
     try {
         const cffResult = await getCitationFile(".");
