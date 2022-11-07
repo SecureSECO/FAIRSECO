f090a42b6e6c4968355e7eb4b93c62c070c3b7ba
Bart Hageman <barthageman1@gmail.com>
2022-11-07 14:08:14 +0100

    FEAT: Add automatic doc generation
    Added automatic type documentation using typedoc

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-import {main} from './action'
+import { main } from "./action";
 
-main()
+main();
+
+/**
+ * Console logs the result of adding a to b
+ * @param a : number 1 to add
+ * @param b : number 2 to add
+ * @returns nothing
+ */
+export function test(a: number, b: number) {
+    console.log(`Stuff:${a + b}`);
+}
