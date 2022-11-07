4e23b4b0f468d1b76cc6733653913e051f5202eb
Bart Hageman <barthageman1@gmail.com>
2022-11-07 14:36:29 +0100

    DOCS: Removed test function/documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
 import { main } from "./action";
 
 main();
-
-/**
- * Console logs the result of adding a to b
- * @param a : number 1 to add
- * @param b : number 2 to add
- * @returns nothing
- */
-export function test(a: number, b: number) {
-    console.log(`Stuff:${a + b}`);
-}
