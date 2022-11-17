20ed1a05ede115b03f34856938c88bba6e7d216f
Bart Hageman <b.hageman@students.uu.nl>
2022-11-17 13:40:25 +0100

    DOCS: Automatic documentation generation (#12)
    * FEAT: Add automatic doc generation
    
    Added automatic type documentation using typedoc
    
    * DOCS: Initial documentation
    
    * FEAT: Added automatic documentation building workflow
    
    * DOCS: Update docs to reflect last push
    
    * DOCS: Removed test function/documentation
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/action.ts
+++ src/action.ts
@@ -1,19 +1,21 @@
 import { pre } from "./pre";
 import { data } from "./getdata";
 import { post } from "./post";
-import * as core from '@actions/core'
+import * as core from "@actions/core";
 
+/**
+ * Entrypoint of the program
+ *
+ * @returns nothing
+ */
 export async function main(): Promise<void> {
     try {
         const check = await pre();
-        if (check){
-
-        const result = await data(); // call data check.  
-        await post(result); // call post check.
-
-
+        if (check) {
+            const result = await data(); // call data check.
+            await post(result); // call post check.
         }
     } catch (error) {
-        core.setFailed(error.message)
+        core.setFailed(error.message);
     }
 }
