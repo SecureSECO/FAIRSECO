9084d6aae03dc470c2ceab8813d25ff10c69a493
Bart Hageman <barthageman1@gmail.com>
2022-12-08 16:13:08 +0100

    Changed tests

--- __tests__/unit_tests/webapp_tests.ts
+++ __tests__/unit_tests/webapp_tests.ts
@@ -1,20 +1,28 @@
-import * as fs from 'fs'
-import { ReturnObject } from '../../src/getdata';
-import {WriteHTML, WriteCSS} from '../../src/webapp'
+import * as fs from "fs";
+// import { ReturnObject } from "../../src/getdata";
+import { WriteHTML } from "../../src/webapp";
 
 test("Check if html file exists", async () => {
-    let fileExists: Boolean = false;
-    let mockResult: ReturnObject[] = []
-    let mockSingleResult: ReturnObject = {
-        ReturnName: "test",
-        ReturnData: {}
-    }
-    mockResult[0] = mockSingleResult;
+    // let fileExists: Boolean = false;
+    // let mockResult: ReturnObject[] = [];
+    // let citingPapers: ReturnObject = {
+    //     ReturnName: "citingPapers",
+    //     ReturnData: [],
+    // };
+    // let tortellini: ReturnObject = {
+    //     ReturnName: "Tortellini",
+    //     ReturnData: {
+    //         violations: [],
+    //     },
+    // };
+    // mockResult[0] = citingPapers;
+    // mockResult[1] = tortellini;
 
-    WriteHTML(mockResult, "./.FairSECO/index.html").then(() => {
-        fileExists = fs.existsSync("./.FairSECO/index.html");
+    let fd = fs.openSync("./__tests__/data/ejstestdata.json", "r");
+    let mockResult = JSON.parse(fs.readFileSync(fd).toString());
+
+    WriteHTML(mockResult.data, "./.FairSECO/index.html").then(() => {
+        let fileExists = fs.existsSync("./.FairSECO/index.html");
         expect(fileExists).toBe(true);
-    }).catch(() => {
-        expect(fileExists).toBe(true);
-    })
+    });
 });
