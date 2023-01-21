f9fb8a5bc3c12ccefc92f45f8a63656d4217095c
Bart Hageman <barthageman1@gmail.com>
2023-01-21 17:06:43 +0100

    FIX: Move test from post test to webapp test

--- __tests__/unit_tests/post_test.ts
+++ __tests__/unit_tests/post_test.ts
@@ -4,27 +4,12 @@ import fs from "fs";
 import testData from '../data/ejstestdata.json'
 import {ReturnObject} from '../../src/getdata'
 
+// Do we really need a test to see if a folder is created? This is not part of our functionality.
+
 // test("Generates .FairSECO folder", async () => {
 //     await post([]);
 //     let FolderExists: Boolean = false;
 //     FolderExists = fs.existsSync("./.FairSECO");
 //     expect(FolderExists).toBe(true);
 // });
-//
-describe('Index.html is still generated with', () => {
-    // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
-    const returnNames : String[] = [];
-    testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
-    
-    test.each(returnNames)('%s ommitted from the results.', async (toBeOmitted) => {
-        const modifiedData: ReturnObject[] = testData.data.filter((obj) => obj.ReturnName !== toBeOmitted);
-        await post(modifiedData) 
-        let FileExists: Boolean = false;
-        FileExists = fs.existsSync("./.FairSECO/index.html");
-        expect(FileExists).toBe(true);
-    });
-});
 
-afterEach(() => {
-    fs.rmSync('./FairSECO', {recursive: true, force: true})
-});
--- __tests__/unit_tests/webapp_tests.ts
+++ __tests__/unit_tests/webapp_tests.ts
@@ -1,28 +1,16 @@
 import * as fs from "fs";
 // import { ReturnObject } from "../../src/getdata";
 import { WriteHTML } from "../../src/webapp";
+import {ReturnObject} from '../../src/getdata'
+import testData from '../data/ejstestdata.json'
 
-test("Check if html file exists", async () => {
-    // let fileExists: Boolean = false;
-    // let mockResult: ReturnObject[] = [];
-    // let citingPapers: ReturnObject = {
-    //     ReturnName: "citingPapers",
-    //     ReturnData: [],
-    // };
-    // let tortellini: ReturnObject = {
-    //     ReturnName: "Tortellini",
-    //     ReturnData: {
-    //         violations: [],
-    //     },
-    // };
-    // mockResult[0] = citingPapers;
-    // mockResult[1] = tortellini;
-
-    let fd = fs.openSync("./__tests__/data/ejstestdata.json", "r");
-    let mockResult = JSON.parse(fs.readFileSync(fd).toString());
-
-    WriteHTML(mockResult.data, "./.FairSECO/index.html").then(() => {
-        let fileExists = fs.existsSync("./.FairSECO/index.html");
-        expect(fileExists).toBe(true);
+describe('Index.html is still generated with', () => {
+    // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
+    const returnNames : String[] = [];
+    testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
+    
+    test.each(returnNames)('%s ommitted from the results.', async (toBeOmitted) => {
+        const modifiedData: ReturnObject[] = testData.data.filter((obj) => obj.ReturnName !== toBeOmitted);
+        return expect(WriteHTML(modifiedData, "./.FairSECO/index.html")).resolves.not.toThrow()
     });
 });
