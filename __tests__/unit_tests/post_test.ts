d6b26cbff6013defc9b007c0f0865d1f27b62574
Bart Hageman <barthageman1@gmail.com>
2023-01-21 16:40:54 +0100

    FIX: Allow json imports, improve comments

--- __tests__/unit_tests/post_test.ts
+++ __tests__/unit_tests/post_test.ts
@@ -1,7 +1,7 @@
 import { post } from "../../src/post";
 import { expect, test } from "@jest/globals";
 import fs from "fs";
-import testData from '../data/ejstestdata.json' assert {type: 'json'}
+import testData from '../data/ejstestdata.json'
 import {ReturnObject} from '../../src/getdata'
 
 // test("Generates .FairSECO folder", async () => {
@@ -11,7 +11,21 @@ import {ReturnObject} from '../../src/getdata'
 //     expect(FolderExists).toBe(true);
 // });
 //
-describe('The html is still generated with', () => {
+describe('Index.html is still generated with', () => {
+    // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
+    const returnNames : String[] = [];
+    testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
+    
+    test.each(returnNames)('%s ommitted from the results.', async (toBeOmitted) => {
+        const modifiedData: ReturnObject[] = testData.data.filter((obj) => obj.ReturnName !== toBeOmitted);
+        await post(modifiedData) 
+        let FileExists: Boolean = false;
+        FileExists = fs.existsSync("./.FairSECO/index.html");
+        expect(FileExists).toBe(true);
+    });
+});
+
+describe('Index.html is still generated with', () => {
     // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
     const returnNames : String[] = [];
     testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
