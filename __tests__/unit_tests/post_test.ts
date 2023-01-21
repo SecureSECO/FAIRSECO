baaa9b5475606a20f52a307067fdb55f78c676bc
Bart Hageman <barthageman1@gmail.com>
2023-01-21 16:43:29 +0100

    FIX: Forgot to remove code i duplicated
    Whoops

--- __tests__/unit_tests/post_test.ts
+++ __tests__/unit_tests/post_test.ts
@@ -25,20 +25,6 @@ describe('Index.html is still generated with', () => {
     });
 });
 
-describe('Index.html is still generated with', () => {
-    // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
-    const returnNames : String[] = [];
-    testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
-    
-    test.each(returnNames)('%s ommitted from the results.', async (toBeOmitted) => {
-        const modifiedData: ReturnObject[] = testData.data.filter((obj) => obj.ReturnName !== toBeOmitted);
-        await post(modifiedData) 
-        let FileExists: Boolean = false;
-        FileExists = fs.existsSync("./.FairSECO/index.html");
-        expect(FileExists).toBe(true);
-    });
-});
-
 afterEach(() => {
     fs.rmSync('./FairSECO', {recursive: true, force: true})
 });
