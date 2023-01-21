495ca9815f93069142342913b24e6bd5df3ebbac
Bart Hageman <barthageman1@gmail.com>
2023-01-21 16:26:09 +0100

    TEST: Omit objects test

--- __tests__/unit_tests/post_test.ts
+++ __tests__/unit_tests/post_test.ts
@@ -1,10 +1,30 @@
 import { post } from "../../src/post";
 import { expect, test } from "@jest/globals";
 import fs from "fs";
+import testData from '../data/ejstestdata.json' assert {type: 'json'}
+import {ReturnObject} from '../../src/getdata'
 
-test("Generates .FairSECO folder", async () => {
-    await post([]);
-    let FolderExists: Boolean = false;
-    FolderExists = fs.existsSync("./.FairSECO");
-    expect(FolderExists).toBe(true);
+// test("Generates .FairSECO folder", async () => {
+//     await post([]);
+//     let FolderExists: Boolean = false;
+//     FolderExists = fs.existsSync("./.FairSECO");
+//     expect(FolderExists).toBe(true);
+// });
+//
+describe('The html is still generated with', () => {
+    // Collect all the returnnames from our test data into an array, so we can test these objects being ommitted
+    const returnNames : String[] = [];
+    testData.data.forEach((obj) => returnNames.push(obj.ReturnName));
+    
+    test.each(returnNames)('%s ommitted from the results.', async (toBeOmitted) => {
+        const modifiedData: ReturnObject[] = testData.data.filter((obj) => obj.ReturnName !== toBeOmitted);
+        await post(modifiedData) 
+        let FileExists: Boolean = false;
+        FileExists = fs.existsSync("./.FairSECO/index.html");
+        expect(FileExists).toBe(true);
+    });
+});
+
+afterEach(() => {
+    fs.rmSync('./FairSECO', {recursive: true, force: true})
 });
